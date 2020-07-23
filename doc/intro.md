15b61d3c8789ba14a23e57ce9c765885611a71d4
samrose <samuel.rose@gmail.com>
2020-07-23 12:19:03 -0400

    clj-mw

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-mw
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_mw/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-mw.core-test
+  (:require [clojure.test :refer :all]
+            [clj-mw.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
